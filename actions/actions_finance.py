8608297f6c0da80ac220a22501d1d7c3c00e0aff
wyt1234 <976582772@qq.com>
2022-04-28 11:34:54 +0800

    补充起购金额和收益率的条件2

--- actions/actions_finance.py
+++ actions/actions_finance.py
@@ -69,29 +69,6 @@ def sentences_similarity(sentences, corpus, topk=3, min_simil=0):
 
 
 #####购买理财产品##########
-class ActionBuyFinancialProducts(Action):
-    """购买理财产品"""
-
-    def name(self) -> Text:
-        """Unique identifier of the action"""
-        return "buy_financial_products"
-
-    async def run(
-            self,
-            dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any],
-    ) -> List[Dict]:
-        """Executes the action"""
-        slots = {
-        }
-        finance_product = tracker.get_slot("finance_product")
-        finance_amount = tracker.get_slot("finance_amount")
-        text = (f"您已成功购买理财产品：{finance_product},购买金额为：{finance_amount}")
-        dispatcher.utter_message(text=text)
-        return []
-
-
 # 验证理财表单（废弃）
 class ValidatePurchaseFinaceForm(FormValidationAction):
     def name(self) -> Text:
@@ -150,17 +127,17 @@ class ActionRecommandFinancialProducts(Action):
         # if not finance_product:
         text = (f"正在为您查询最新的理财产品...")
         dispatcher.utter_message(text=text)
-        text = (f"根据您的喜好，给您推荐以下几款产品：\n"
-                f"-鑫增利添添盈A款，预期年化收益率2.5% \n"
-                f"-鑫增利添添盈B款，预期年化收益率2.3% \n"
-                f"-幸福货币基金C款，预期年化收益率2.7% "
-                )
-        dispatcher.utter_message(text=text)
+        # text = (f"根据您的喜好，给您推荐以下几款产品：\n"
+        #         f"-鑫增利添添盈A款，预期年化收益率2.5% \n"
+        #         f"-鑫增利添添盈B款，预期年化收益率2.3% \n"
+        #         f"-幸福货币基金C款，预期年化收益率2.7% "
+        #         )
+        # dispatcher.utter_message(text=text)
         return []
 
 
 # 详细介绍理财产品
-class ActionRecommandFinancialProducts(Action):
+class ActionFinancialProductsDetail(Action):
 
     def name(self) -> Text:
         """Unique identifier of the action"""
@@ -177,5 +154,28 @@ class ActionRecommandFinancialProducts(Action):
             text = (f"正在为您详细介绍XXX产品")
         else:
             text = (f"正在为您详细介绍全部产品")
+        dispatcher.utter_message(text=text)
+        return []
+
+
+class ActionBuyFinancialProducts(Action):
+    """确认下单"""
+    def name(self) -> Text:
+        """Unique identifier of the action"""
+        return "buy_financial_products"
+
+    async def run(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> List[Dict]:
+        """Executes the action"""
+        slots = {
+        }
+        # finance_product = tracker.get_slot("finance_product")
+        # finance_amount = tracker.get_slot("finance_amount")
+        # text = (f"您已成功购买理财产品：{finance_product},购买金额为：{finance_amount}")
+        text = (f"您已成功购买理财产品")
         dispatcher.utter_message(text=text)
         return []
